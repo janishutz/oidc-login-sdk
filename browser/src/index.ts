374e1dfc9ee027287d5eda4e4ca10c1dc142498e
Janis Hutz <development@janishutz.com>
2026-09-21 14:39:56 +0200

    fix: browser sdk did not export some types

--- browser/src/index.ts
+++ browser/src/index.ts
@@ -1,6 +1,10 @@
 import config from './config.js';
 
+export type * as dtype from './dtype.d.ts';
+
 export const configure = config.configure;
+
+export * as auth from './auth.js';
 
 /**
  * Wrapper on top of fetch, with redirects from the backend handled according to your rules
